286584242786f00364e8b5e338718418d7a65139
Llorch <SkylakeBeta@gmail.com>
2016-09-30 20:54:22 -0700

    Solve No Duplicate in constant space

--- Practice/NoDuplicate.py
+++ Practice/NoDuplicate.py
@@ -1,6 +1,15 @@
+# O(n) space complexity solution
 def remove_duplicate(arr):
 	result = []
 	for num in arr:
 		if num not in result:
 			result.append(num)
 	return len(result)
+
+# O(1) space complexity solution
+def remove_duplicate(arr):
+	lenght = 1
+	for i in range(len(arr)-1):
+		if arr[i] != arr[i+1]:
+			lenght += 1
+	return lenght
