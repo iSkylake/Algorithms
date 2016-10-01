387ef658aec76935ba11d1f3d4cacd09fb79f225
Llorch <SkylakeBeta@gmail.com>
2016-10-01 15:39:50 -0700

    Finish Linked List Reversal and add cleaner version

--- Practice/LinkedListReverse.py
+++ Practice/LinkedListReverse.py
@@ -9,15 +9,34 @@ class LinkedList:
 		self.tail = None
 		self.length = 0
 
-	def insert(self, val):
+	def append(self, val):
 		new_node = Node(val)
 		if self.length == 0:
 			self.head = new_node
+		else:
+			self.tail.next = new_node
 		self.tail = new_node
 		self.length += 1
 
+# First Attempt
 def reverse_list(node):
 	previous = None
 	current = node
 	next_node = node.next
-	
+	while current != None:
+		current.next = previous
+		previous = current
+		current = next_node
+		if current != None:
+			next_node = current.next
+
+# Cleaner
+def reverse_list(node):
+	previous = None
+	current = node
+	next_node = node.next
+	while current != None:
+		next_node = current.next
+		current.next = previous
+		previous = current
+		current = next_node
