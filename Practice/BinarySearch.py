64f7b0897fb5926d096a0e5c00cb55b920d5a322
Llorch <SkylakeBeta@gmail.com>
2016-10-07 19:31:25 -0700

    Finish recursive binary search, iteration pending

--- Practice/BinarySearch.py
+++ Practice/BinarySearch.py
@@ -1,13 +1,15 @@
 def binary_search(arr, target):
-	mid = len(arr)//2
-	print(mid)
-	if arr[mid] == target:
-		return True
-	else:
-		if arr[mid] < target:
-			binary_search(arr[mid:], target)
+	if len(arr) > 0:
+		mid = len(arr)//2
+		print(arr[mid])
+		if arr[mid] == target:
+			return True
+		if target < arr[mid]:
+			return binary_search(arr[:mid], target)
 		else:
-			binary_search(arr[:mid], target)
-	return False
+			return binary_search(arr[mid+1:], target)
+	else:
+		return False
 
-print(binary_search([1, 2, 3, 4, 5, 6, 7, 8], 5))
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8], 1))
+
