6d809f310ae19c8d9912a19e5c94a5c668b63970
Llorch <SkylakeBeta@gmail.com>
2016-10-13 19:01:22 -0700

    Finish Kth Largest in Array, iterative

--- Array/KthLargestinArray.py
+++ Array/KthLargestinArray.py
@@ -1,14 +1,27 @@
 def kth_large_in_array(arr, k):
-	if k > len(arr) - 1:
+	if k > len(arr):
 		return False
-	mid = len(arr)//2
-	pivot = arr[mid]
-	left = 0
-	right = len(arr) - 1
-	while left < right:
-		while arr[left] < pivot:
-			left += 1
-		while arr[right] > pivot:
-			right += 1
-		if arr[left] > arr[right]:
-			arr[left], arr[right] = arr[right], arr[left]
+	new_k = len(arr) - k
+	first = 0
+	last = len(arr) - 1
+	found = False
+	while not found:
+		mid = (first + last)//2
+		left = first
+		right = last
+		pivot = arr[mid]
+		while left < right:
+			while arr[left] < pivot:
+				left += 1
+			while arr[right] > pivot:
+				right -= 1
+			if arr[left] > arr[right]:
+				arr[left], arr[right] = arr[right], arr[left]
+		if arr.index(pivot) == new_k:
+			return pivot
+		elif new_k > arr.index(pivot):
+			first = arr.index(pivot) + 1
+		else:
+			last = arr.index(pivot) - 1
+
+print(kth_large_in_array([3, 1, 6, 4, 9, 8], 1))
