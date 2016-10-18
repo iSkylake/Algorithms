724a6d0471992b4fb97a5799bbffbdb0832e4995
Llorch <SkylakeBeta@gmail.com>
2016-10-18 10:23:04 -0700

    Move Binary Search to Sort and Search directory

--- Array/SortedSubString.py
+++ Array/SortedSubString.py
@@ -6,14 +6,14 @@ def sort_sub_string(arr):
 		if arr[i] > max_val:
 			max_val = arr[i]
 
-		if arr[i] < max_val:
+		if max_val < arr[i]:
 			right = i
 
 	for i in range(len(arr)-1, -1, -1):
 		if arr[i] < min_val:
 			min_val = arr[i]
 
-		if arr[i] > min_val:
+		if min_val < arr[i]:
 			left = i
 
 	return [left, right]
