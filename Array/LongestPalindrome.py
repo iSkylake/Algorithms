8844737bc66980f50698254a01da577b9391f625
Llorch <SkylakeBeta@gmail.com>
2016-10-25 23:32:55 -0700

    Add even palindrome verificator

--- Array/LongestPalindrome.py
+++ Array/LongestPalindrome.py
@@ -17,7 +17,7 @@
 	# 			left = -1
 
 def longestPalindrome(string):
-	max_lenght = 0
+	max_odd = 0
 	s = string.lower()
 	n = len(s)
 	for i in range(n):
@@ -29,9 +29,25 @@ def longestPalindrome(string):
 				left -= 1
 				right += 1
 				count += 2
-				max_lenght = max(count, max_lenght)
+				max_odd = max(count, max_odd)
 			else:
 				left = -1
-	return max_lenght
+
+	max_even = 0
+	for i in range(n):
+		left = i
+		right = i + 1
+		count = 0
+		while left >= 0 and right < n:
+			if s[left] == s[right]:
+				left -= 1
+				right += 1
+				count += 2
+				max_even = max(count, max_even)
+			else:
+				left = -1	
+
+	return max(max_odd, max_even)
 
 print(longestPalindrome("monkeybananacarsaibohphobialoool"))
+print(longestPalindrome("bananaAButTuba"))
