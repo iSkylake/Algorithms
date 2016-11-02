ed8781669874531328bee7db1124f91661ee6910
Llorch <SkylakeBeta@gmail.com>
2016-11-01 23:40:14 -0700

    Add function with string vowels in Most Vowel

--- Array/MostVowel.py
+++ Array/MostVowel.py
@@ -1,4 +1,4 @@
-def most_vowel(array):
+def most_vowel_array(array):
 	vowels = ['a', 'e', 'i', 'o', 'u']
 	max_vowel = [0, 0]
 	for string in range(len(array)):
@@ -10,4 +10,21 @@ def most_vowel(array):
 			max_vowel[0] = string
 			max_vowel[1] = vowel_count
 
-	return max_vowel[0]
+	return str(max_vowel[0])
+
+def most_vowel_string(array):
+	vowels = 'aeiou'
+	max_vowel = [0, 0]
+	for string in range(len(array)):
+		vowel_count = 0
+		for letter in array[string]:
+			if letter in vowels:
+				vowel_count += 1
+		if vowel_count > max_vowel[1]:
+			max_vowel[0] = string
+			max_vowel[1] = vowel_count
+
+	return str(max_vowel[0])
+
+print("The word with more vowels is in the position " + most_vowel_array(['hello', 'hello world', 'python']))
+print("The word with more vowels is in the position " + most_vowel_string(['hello', 'hello world', 'aeiou']))
