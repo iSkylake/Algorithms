ededf0dc488807863934b59a7d623b9bc686595d
Llorch <skylakebeta@gmail.com>
2016-11-21 23:31:53 -0800

    Finish Lowest Common Ancestor

--- Tree/LowestCommonAncestor.py
+++ Tree/LowestCommonAncestor.py
@@ -4,22 +4,65 @@ class Node:
 		self.left = None
 		self.right = None
 
-def lowest_common_ancestor(root, node1, node2):
+# def lowest_common_ancestor(root, node1, node2):
 
-	node_list_1, node_list_2 = [], []
+# 	node_list_1, node_list_2 = [], []
 
-	def traverse(node, node1):
-		nonlocal node_list_1
-		if node == None:
+# 	def traverse(node, node1):
+# 		nonlocal node_list_1
+# 		if node == None:
+# 			return
+# 		else:
+# 			node_list_1.append(node.value)
+# 			if node.value == node1:
+# 				return
+# 			traverse(node.left, node1)
+# 			traverse(node.right, node1)
+# 			node_list_1.pop()
+
+# 	traverse(root, node1)
+
+# 	print(node_list_1)
+
+def lowest(root, first_value, second_value):
+	first_search = []
+	second_search = []
+	found1 = False
+	found2 = False
+	
+	def traverse(node, first_value):
+		nonlocal first_search
+		nonlocal found1
+		if node == None or found1:
 			return
-		else:
-			node_list_1.append(node.value)
-			if node.value == node1:
-				return
-			traverse(node.left, node1)
-			traverse(node.right, node1)
-			node_list_1.pop()
+		
+		first_search.append(node.value)
+		if node.value == first_value:
+			found1 = True
+		traverse(node.left, first_value)
+		traverse(node.right, first_value)
+		if not found1:
+			first_search.pop()
+	traverse(root, first_value)
+	# print(first_search)
 
-	traverse(root, node1)
+	def traverse(node, second_value):
+		nonlocal second_search
+		nonlocal found2
+		if node == None or found2:
+			return
+		
+		second_search.append(node.value)
+		if node.value == second_value:
+			found2 = True
+		traverse(node.left, second_value)
+		traverse(node.right, second_value)
+		if not found2:
+			second_search.pop()
+	traverse(root, second_value)
 
-	print(node_list_1)
+	for i in range(len(second_search)):
+		if second_search[i] in first_search:
+			lowest_common_ancestor = second_search[i]
+
+	return lowest_common_ancestor
