d8809f9a3e137542fdb223e019c1e9c14edbd580
Llorch <SkylakeBeta@gmail.com>
2016-11-29 16:00:24 -0800

    Refactor all directories to Python directory

--- Python/Tree/LowestCommonAncestor.py
+++ Python/Tree/LowestCommonAncestor.py
@@ -4,26 +4,6 @@ class Node:
 		self.left = None
 		self.right = None
 
-# def lowest_common_ancestor(root, node1, node2):
-
-# 	node_list_1, node_list_2 = [], []
-
-# 	def traverse(node, node1):
-# 		nonlocal node_list_1
-# 		if node == None:
-# 			return
-# 		else:
-# 			node_list_1.append(node.value)
-# 			if node.value == node1:
-# 				return
-# 			traverse(node.left, node1)
-# 			traverse(node.right, node1)
-# 			node_list_1.pop()
-
-# 	traverse(root, node1)
-
-# 	print(node_list_1)
-
 def lowest(root, first_value, second_value):
 	first_search = []
 	second_search = []
