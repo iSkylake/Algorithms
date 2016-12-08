22ee44d2e047b92387a6f145da52e60b21f27846
Llorch <SkylakeBeta@gmail.com>
2016-12-07 23:05:21 -0800

    Clean Addition Balance

--- Python/Array/AdditionBalance.py
+++ Python/Array/AdditionBalance.py
@@ -1,14 +1,28 @@
-def solution(A):
+# def add_balance(A):
+# 	left = 0
+# 	result = []
+# 	for i in range(1, len(A)):
+# 		left += A[i-1]
+# 		total = 0
+# 		for j in range(i+1, len(A)):
+# 			total += A[j]
+# 		if total == left:
+# 			result.append(i)
+# 	print(result)
+
+# add_balance([-1, 3, -4, 5, 1, -6, 2, 1])
+
+
+
+def add_balance(A):
 	left = 0
+	right = sum(A[1:])
 	result = []
 	for i in range(1, len(A)):
 		left += A[i-1]
-		total = 0
-		for j in range(i+1, len(A)):
-			total += A[j]
-		print("%s %s" %(total, left))
-		if total == left:
+		right -= A[i]
+		if left == right:
 			result.append(i)
 	print(result)
 
-solution([-1, 3, -4, 5, 1, -6, 2, 1])
+add_balance([-1, 3, -4, 5, 1, -6, 2, 1])
