8e85572e6ef8ab76ba8c3791c2fb5dfdac1283ec
Llorch <SkylakeBeta@gmail.com>
2016-12-08 16:40:36 -0800

    Fix Addition Balance and add test

--- Python/Array/AdditionBalance.py
+++ Python/Array/AdditionBalance.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 # def add_balance(A):
 # 	left = 0
 # 	result = []
@@ -13,16 +15,24 @@
 # add_balance([-1, 3, -4, 5, 1, -6, 2, 1])
 
 
-
 def add_balance(A):
 	left = 0
-	right = sum(A[1:])
+	right = sum(A)
 	result = []
-	for i in range(1, len(A)):
-		left += A[i-1]
+	for i in range(0, len(A)):
+		if i > 0:
+			left += A[i-1]
 		right -= A[i]
 		if left == right:
 			result.append(i)
-	print(result)
+	return result
 
-add_balance([-1, 3, -4, 5, 1, -6, 2, 1])
+print(add_balance([-1, 3, -4, 5, 1, -6, 2, 1]))
+
+class Add_Balance(object):
+	def test(self, func):
+		assert_equal(func([-1, 3, -4, 5, 1, -6, 2, 1]), [1, 3, 7])
+		print("TEST PASSED")
+
+t = Add_Balance()
+t.test(add_balance)
