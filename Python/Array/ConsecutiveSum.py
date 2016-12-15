bd169b292438a49ed6e960b4c6c877bf4edc236d
Llorch <SkylakeBeta@gmail.com>
2016-12-14 23:59:36 -0800

    Add test to Consecutive Sum

--- Python/Array/ConsecutiveSum.py
+++ Python/Array/ConsecutiveSum.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def consecutive_sum(arr, target):
 	total = 0
 	for num in arr:
@@ -8,3 +10,15 @@ def consecutive_sum(arr, target):
 		else:
 			total = num
 	return False
+
+class Consecutive_Sum(object):
+	def test(self, func):
+		assert_equal(func([1, 1, 5, 8], 2), True)
+		assert_equal(func([1, 1, 5, 8], 6), True)
+		assert_equal(func([1, 6, 2, 10], 1), True)
+		assert_equal(func([1, 6, 2, 10], 20), False)
+		assert_equal(func([5, 10, 2, 9], 26), True)
+		print("TESTS PASSED")
+
+t = Consecutive_Sum()
+t.test(consecutive_sum)
