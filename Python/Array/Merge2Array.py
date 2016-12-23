bb0a6c3fb29fd0676555f485ba4eec2da30d372c
Llorch <SkylakeBeta@gmail.com>
2016-12-22 23:41:21 -0800

    Fix Merge 2 Array and add test

--- Python/Array/Merge2Array.py
+++ Python/Array/Merge2Array.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def merge_2_array(arr1, arr2):
 	result = []
 	i, j = 0, 0
@@ -9,17 +11,25 @@ def merge_2_array(arr1, arr2):
 			result.append(arr2[j])
 			j += 1
 	
-	# while i < len(arr1):
-	# 	result.append(arr1[i])
-	# 	i += 1
+	while i < len(arr1):
+		result.append(arr1[i])
+		i += 1
 
-	# while j < len(arr2):
-	# 	result.append(arr2[j])
-	# 	j += 1
+	while j < len(arr2):
+		result.append(arr2[j])
+		j += 1
 
-	if i > j:
-		result + arr1
-	else:
-		result + arr2
+	# if i > j:
+	# 	result + arr1
+	# else:
+	# 	result + arr2
 
 	return result
+
+class Merge_2_Array(object):
+	def test(self, func):
+		assert_equal(func([1, 5, 7, 8], [2, 3, 4, 10]), [1, 2, 3, 4, 5, 7, 8, 10])
+		print("TEST PASSED")
+
+t = Merge_2_Array()
+t.test(merge_2_array)
