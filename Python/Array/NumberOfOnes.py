7c62983a9bfe8837d45ee02b1cbc80bcb572cf59
Llorch <SkylakeBeta@gmail.com>
2016-12-22 23:54:19 -0800

    Add test to Number of Ones

--- Python/Array/NumberOfOnes.py
+++ Python/Array/NumberOfOnes.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def number_of_ones(arr):
 	first = 0
 	last = len(arr) - 1
@@ -10,3 +12,12 @@ def number_of_ones(arr):
 		else:
 			last = mid - 1
 	return 0
+
+class Number_of_Ones(object):
+	def test(self, func):
+		assert_equal(func([0, 0, 0, 1, 1, 1]), 3)
+		assert_equal(func([0, 0, 0, 0, 0, 0, 0, 0]), 0)
+	print('TESTS PASSED')
+
+t = Number_of_Ones()
+t.test(number_of_ones)
