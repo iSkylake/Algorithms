ebfdf627aab09c2c61befccd65064bcd5f926113
Llorch <SkylakeBeta@gmail.com>
2016-12-23 23:53:08 -0800

    Add test to Bubble Sort

--- Python/Sort and Search/BubbleSort.py
+++ Python/Sort and Search/BubbleSort.py
@@ -1,6 +1,17 @@
+from nose.tools import assert_equal
+
 def bubbleSort(arr):
 	for i in range(len(arr)):
 		for j in range(i+1, len(arr)):
 			if arr[j] < arr[i]:
 				arr[i], arr[j] = arr[j], arr[i]
 	return arr
+
+class Bubble_Sort(object):
+	def test(self, func):
+		assert_equal(func([1, 7, 4, 2, 10, 8, 20, 3]), [1, 2, 3, 4, 7, 8, 10, 20])
+		assert_equal(func([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+		print('TESTS PASSED')
+
+t = Bubble_Sort()
+t.test(bubbleSort)
