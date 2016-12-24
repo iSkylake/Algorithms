dabdff9b5b270e451fd5bd0a9cdb134aae0c3a4f
Llorch <SkylakeBeta@gmail.com>
2016-12-23 23:49:47 -0800

    Add test to Binary Search

--- Python/Sort and Search/BinarySearch.py
+++ Python/Sort and Search/BinarySearch.py
@@ -1,21 +1,23 @@
 # Implementation of a Binary Search
 
+from nose.tools import assert_equal
+
 # Using recursion
-def binary_search(arr, target):
+def binary_search_recursion(arr, target):
 	if len(arr) > 0:
 		mid = len(arr)//2
 		print(arr[mid])
 		if arr[mid] == target:
 			return True
 		if target < arr[mid]:
-			return binary_search(arr[:mid], target)
+			return binary_search_recursion(arr[:mid], target)
 		else:
-			return binary_search(arr[mid+1:], target)
+			return binary_search_recursion(arr[mid+1:], target)
 	else:
 		return False
 
 # Using iteration
-def binary_search(arr, target):
+def binary_search_iteration(arr, target):
 	first = 0
 	last = len(arr) - 1
 	while first <= last:
@@ -29,5 +31,17 @@ def binary_search(arr, target):
 				first = mid + 1
 	return False
 
-print(binary_search([1, 2, 3, 4, 5, 6, 7, 8], 1))
+# print(binary_search([1, 2, 3, 4, 5, 6, 7, 8], 1))
 
+class Binary_Search(object):
+	def test(self, func):
+		assert_equal(func([1, 2, 3, 4, 5, 6, 7, 8], 1), True)
+		assert_equal(func([1, 2, 3, 4, 5, 6, 7, 8], 4), True)
+		assert_equal(func([1, 2, 3, 4, 5, 6, 7, 8], 0), False)
+		assert_equal(func([1, 2, 3, 4, 5, 6, 7, 8], 9), False)
+		assert_equal(func([1, 2, 3, 4, 6, 7, 8], 5), False)
+		print('TESTS PASSED')
+
+t = Binary_Search()
+t.test(binary_search_recursion)
+t.test(binary_search_iteration)
