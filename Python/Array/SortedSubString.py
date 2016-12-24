268ef9557a6021a56af1391570abeed3fa16efed
Llorch <SkylakeBeta@gmail.com>
2016-12-23 23:36:18 -0800

    Add test  to Sort Substring

--- Python/Array/SortedSubString.py
+++ Python/Array/SortedSubString.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def sort_sub_string(arr):
 	left, max_val = 0, arr[0]
 	right, min_val = len(arr) - 1, arr[len(arr) - 1]
@@ -20,3 +22,12 @@ def sort_sub_string(arr):
 
 # [3,4,8,7,10,6,17]
 # [3,4,8,7,20,6,17]
+
+class Sort_Sub_String(object):
+	def test(self, func):
+		assert_equal(func([3, 4, 8, 7, 10, 6, 17]), [2, 5])
+		assert_equal(func([3, 4, 8, 7, 20, 6, 17]), [2, 6])
+		print('TEST PASSED')
+
+t = Sort_Sub_String()
+t.test(sort_sub_string)
