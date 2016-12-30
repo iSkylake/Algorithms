c815b4df56f8181380ee7554c6f95b37f005b5bd
Llorch <SkylakeBeta@gmail.com>
2016-12-29 16:03:24 -0800

    Add test to Selection Sort

--- Python/Sort and Search/SelectionSort.py
+++ Python/Sort and Search/SelectionSort.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def selectionSort(arr):
 	for i in range(len(arr)):
 		for j in range(i+1, len(arr)):
@@ -7,3 +9,12 @@ def selectionSort(arr):
 			arr[i], arr[minIndex] = arr[minIndex], arr[i]
 			print(arr)
 	return arr
+
+class Selection_Sort(object):
+	def test(self, func):
+		assert_equal(func([1, 7, 4, 2, 10, 8, 20, 3]), [1, 2, 3, 4, 7, 8, 10, 20])
+		assert_equal(func([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+		print('TESTS PASSED')
+
+t = Selection_Sort()
+t.test(selectionSort)
