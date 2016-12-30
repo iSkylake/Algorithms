b72dafe6baa90af7d2f0ae3987d3cd6f7d0d83e9
Llorch <SkylakeBeta@gmail.com>
2016-12-29 16:02:01 -0800

    Add test to Merge Sort

--- Python/Sort and Search/MergeSort.py
+++ Python/Sort and Search/MergeSort.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def mergeSort(arr):
 	if len(arr) == 1:
 		return arr
@@ -31,3 +33,12 @@ def mergeSort(arr):
 			k += 1
 
 	return arr
+
+class Merge_Sort(object):
+	def test(self, func):
+		assert_equal(func([1, 7, 4, 2, 10, 8, 20, 3]), [1, 2, 3, 4, 7, 8, 10, 20])
+		assert_equal(func([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+		print('TESTS PASSED')
+
+t = Merge_Sort()
+t.test(mergeSort)
