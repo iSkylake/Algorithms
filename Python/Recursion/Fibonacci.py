dbd3668de67e7f3b86f95f8a2f2e911ca2d18666
Llorch <SkylakeBeta@gmail.com>
2016-12-30 23:10:03 -0800

    Add test to Fibonnacio

--- Python/Recursion/Fibonacci.py
+++ Python/Recursion/Fibonacci.py
@@ -1,5 +1,18 @@
+from nose.tools import assert_equal
+
 def fibonacci(n):
 	if n <= 1:
 		return n
 	else:
 		return fibonacci(n-1) + fibonacci(n-2)
+
+class Fibonacci:
+	def test(self, func):
+		assert_equal(func(6), 8)
+		assert_equal(func(10), 55)
+		assert_equal(func(0), 0)
+		assert_equal(func(1), 1)
+	print("TESTS PASSED")
+
+t = Fibonacci()
+t.test(fibonacci)
