ff4cf96114fb54d650cdf6d929b13f38a5f29cd8
Llorch <SkylakeBeta@gmail.com>
2017-01-01 23:38:11 -0800

    Add test to Two Sum

--- Python/Array/TwoSum.py
+++ Python/Array/TwoSum.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def two_sum(arr, target):
 	dict_sum = {}
 	for i in range(len(arr)):
@@ -9,3 +11,12 @@ def two_sum(arr, target):
 			return result
 	result = [-1, -1]
 	return result
+
+class Two_Sum(object):
+	def test(self, func):
+		assert_equal(func([1, 6, -5, 7, 3], -2), [2, 4])
+		assert_equal(func([1, 9, 10], 8), [-1, -1])
+		print("TESTS PASSED")
+
+t = Two_Sum()
+t.test(two_sum)
