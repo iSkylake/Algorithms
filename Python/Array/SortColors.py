c678aba3d82857833e99c94143d9becc807b5e50
Llorch <SkylakeBeta@gmail.com>
2017-01-01 23:48:50 -0800

    Add test to Sort Colors

--- Python/Array/SortColors.py
+++ Python/Array/SortColors.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def sort_colors(arr):
 	colors = [0, 0, 0]
 	for num in arr:
@@ -10,3 +12,12 @@ def sort_colors(arr):
 			j += 1
 			colors[i] -= 1
 	return arr
+
+class Sort_Colors(object):
+	def test(self, func):
+		assert_equal(func([2, 0, 1, 2, 1, 0]), [0, 0, 1, 1, 2, 2])
+		assert_equal(func([1, 0, 2]), [0, 1, 2])
+		print("TESTS PASSED")
+
+t = Sort_Colors()
+t.test(sort_colors)
