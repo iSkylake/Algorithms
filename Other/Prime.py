9100d89ed08b17b9f98548458aa5b1088aaee375
Llorch <SkylakeBeta@gmail.com>
2017-01-24 23:45:32 -0800

    Add test to Prime

--- Other/Prime.py
+++ Other/Prime.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def prime(num):
 	primes = [2, 3, 5, 7]
 	count = 0
@@ -11,9 +13,15 @@ def prime(num):
 	else:
 		return False
 
-print(prime(2))
-print(prime(7))
-print(prime(11))
-print(prime(41))
-print(prime(4))
-print(prime(50))
+class Prime(object):
+	def test(self, func):
+		assert_equal(func(2), True)
+		assert_equal(func(7), True)
+		assert_equal(func(11), True)
+		assert_equal(func(41), True)
+		assert_equal(func(4), False)
+		assert_equal(func(50), False)
+		print("TESTS PASSED")
+
+t = Prime()
+t.test(prime)
