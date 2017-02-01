977ba937f8004209801073aa77719e3dfa78969b
Llorch <SkylakeBeta@gmail.com>
2017-01-31 23:59:11 -0800

    Finish SelectionSort JS

--- Sort and Search/SelectionSort.js
+++ Sort and Search/SelectionSort.js
@@ -7,5 +7,13 @@ function selectionSort(array){
 				max = j;
 			}
 		}
+		var temp = array[len-1-i];
+		array[len-i-1] = array[max];
+		array[max] = temp;
 	}
+	return array;
 }
+
+console.log(selectionSort([6,2,4,1,9]));
+console.log(selectionSort([1,2,3,4]));
+console.log(selectionSort([9,8,7,6,5,4,3,2,1]));
