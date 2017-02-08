a0a816d109fa2fb8684e477b6f27bac57811cc76
Llorch <SkylakeBeta@gmail.com>
2017-02-07 22:51:38 -0800

    Add recursive binary search to Binary Search JS

--- Sort and Search/BinarySearch.js
+++ Sort and Search/BinarySearch.js
@@ -14,6 +14,21 @@ function binarySearch(arr, n){
 	return false;
 }
 
+function recBinSearch(arr, n){
+	mid = Math.floor(arr.length/2);
+	if(arr.length < 1){
+		return false;
+	}
+	if(arr[mid] === n){
+		return true;
+	}
+	if(n < arr[mid]){
+		return recBinSearch(arr.slice(0, mid), n);
+	} else{
+		return recBinSearch(arr.slice(mid+1, arr.length), n);
+	}
+}
+
 console.log(binarySearch([1, 2, 4, 5, 6, 7], 1));
 console.log(binarySearch([1, 2, 4, 5, 6, 7], 2));
 console.log(binarySearch([1, 2, 4, 5, 6, 7], 4));
@@ -23,3 +38,15 @@ console.log(binarySearch([1, 2, 4, 5, 6, 7], 7));
 console.log(binarySearch([1, 2, 4, 5, 6, 7], 0));
 console.log(binarySearch([1, 2, 4, 5, 6, 7], 8));
 console.log(binarySearch([1, 2, 5, 6, 7], 18));
+
+console.log(" ");
+
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 1));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 2));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 4));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 5));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 6));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 7));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 0));
+console.log(recBinSearch([1, 2, 4, 5, 6, 7], 8));
+console.log(recBinSearch([1, 2, 5, 6, 7], 18));
