485ab3a036944d12e4324ea46fa341490eb7756a
Llorch <SkylakeBeta@gmail.com>
2017-02-08 23:24:23 -0800

    Fix Valid Circular Linked List PY

--- Linked List/ValidCircularLinkedList.py
+++ Linked List/ValidCircularLinkedList.py
@@ -8,7 +8,7 @@ class Node:
 def valid_circular_LL(node):
 	slow = node
 	fast = node
-	while fast.value != None and fast.next != None:
+	while fast.next.next != None and fast.next != None:
 		slow = slow.next
 		fast = fast.next.next
 		if slow == fast:
