39171e37507599ca14c9716cc04454c4583b0f91
Llorch <SkylakeBeta@gmail.com>
2017-02-21 22:57:03 -0800

    Add Palindrome PY

---
+++ Array/Palindrome.py
@@ -0,0 +1,35 @@
+# class Palindrome:
+# 	def __init__(self):
+# 		self.stack = []
+# 		self.queue = []
+
+# 	def check(self, s):
+# 		if len(s)%2 == 0:
+# 			return self.oddPalindrome(s)
+# 		else:
+# 			return self.evenPalindrome(s)
+
+# 	def oddPalindrome(s):
+# 		
+
+def palindrome(s):
+	size = len(s)
+	mid = size//2
+	if size%2 == 0:
+		left = mid - 1
+		right = mid
+	else:
+		left = mid - 1
+		right = mid + 1
+
+	while left >= 0 and right < size:
+		if s[left] != s[right]:
+			return False
+		left -= 1
+		right += 1
+
+	return True
+
+print(palindrome("palindrome"))
+print(palindrome("abccba"))
+print(palindrome("abcba"))
