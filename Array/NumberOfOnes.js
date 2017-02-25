3599830b5e384ffdc09fd0ce3ee2f25ed489467d
Llorch <SkylakeBeta@gmail.com>
2017-02-25 00:03:42 -0800

    Fix NumberOfOnes JS

--- Array/NumberOfOnes.js
+++ Array/NumberOfOnes.js
@@ -1,23 +1,22 @@
 function numOfOnes(arr){
-	var first = 0;
+	var first = 0; 
 	var last = arr.length;
 	var mid = Math.floor((first+last)/2);
-	while(arr[mid] !== 1 && arr[mid-1] !== 0){
+	while(arr[mid] !== 1 || arr[mid-1] !== 0){
 		if(arr[mid]===0){
 			first = mid + 1;
 		} else{
 			last = mid - 1;
 		}
 		mid = Math.floor((first+last)/2);
-		console.log(mid);
 	}
 	return arr.length - mid;
 }
 
-// console.log(numOfOnes([0, 0, 1, 1]));
-console.log(numOfOnes([0, 0, 0, 1, 1]));
-// console.log(numOfOnes([0, 0, 1, 1, 1]));
-// console.log(numOfOnes([0, 0, 0, 0, 0, 1, 1]));
-// console.log(numOfOnes([0, 1, 1, 1, 1]));
-// console.log(numOfOnes([0, 0, 1]));
-// console.log(numOfOnes([0, 1]));
+console.log(numOfOnes([0, 0, 1, 1])); // 2
+console.log(numOfOnes([0, 0, 0, 1, 1])); // 2
+console.log(numOfOnes([0, 0, 1, 1, 1])); // 3
+console.log(numOfOnes([0, 0, 0, 0, 0, 1, 1])); //2
+console.log(numOfOnes([0, 1, 1, 1, 1])); // 4
+console.log(numOfOnes([0, 0, 1])); // 1
+console.log(numOfOnes([0, 1])); // 1
