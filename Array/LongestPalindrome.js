46d319345812b612c19e255ce4f300168679d289
Llorch <SkylakeBeta@gmail.com>
2017-02-26 23:11:27 -0800

    Fix Longest Palindrome JS

--- Array/LongestPalindrome.js
+++ Array/LongestPalindrome.js
@@ -1,23 +1,42 @@
 function longPalindrome(s){
-	var palIndex = [];
+	var maxIndex = [];
 	var maxPal = 0;
+	// Check for odd palindromes
 	for(i=1; i<s.length; i++){
-		var l = i--;
-		var r = i++;
-		while(l >= 0){
-			var current = 0;
-			if(s[l] === s[r] && current >= palIndex){
-				palIndex = [l, r];
-				console.log(palIndex);
-				maxPal = current;
-				
-			}
+		var palIndex = [];
+		var current = 1;
+		var l = i - 1;
+		var r = i + 1;
+		while(s[l] === s[r] && l >= 0){
+			current += 2;
+			palIndex = [l, r];
 			l--;
 			r++;
 		}
+		if(current > maxPal){
+			maxPal = current;
+			maxIndex = palIndex;
+		}
 	}
-	maxPal++;
-	return s.substring(palIndex[0], palIndex[1]+1);
+	// Check for even palindromes
+	for(i=1; i<s.length; i++){
+		var palIndex = [];
+		var current = 0;
+		var l = i;
+		var r = i + 1;
+		while(s[l] === s[r] && l >= 0){
+			current += 2;
+			palIndex = [l, r];
+			l--;
+			r++;
+		}
+		if(current > maxPal){
+			maxPal = current;
+			maxIndex = palIndex;
+		}
+	}
+	return s.substring(maxIndex[0], maxIndex[1]+1);
 }
 
 console.log(longPalindrome("becauseabbcbbanot"));
+console.log(longPalindrome("becauseabbccbbanot"));
