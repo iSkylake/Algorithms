bf54a163b4857d1795b5b0b654bb8712fa42e3be
Llorch <SkylakeBeta@gmail.com>
2017-02-28 16:04:55 -0800

    Fix One Matrix PY

--- Array/OneMatrix.py
+++ Array/OneMatrix.py
@@ -2,18 +2,25 @@ matrix = [
 	[0, 0, 0, 1],
 	[0, 0, 1, 1],
 	[0, 1, 1, 1],
-	[1, 1, 1, 1]
+	[0, 1, 1, 1]
 ]
 
 def oneMatrix(matrix):
-	i = count = 0
-	j = col = len(matrix[0]) - 1;
-	while i <  len(matrix):
+	count = 0
+	i = 0;
+	j = len(matrix[0]) - 1
+	col = len(matrix[0]) - 1
+
+	while i < len(matrix):
 		if matrix[i][j] == 0 or j == 0:
-			count = count + col - j
+			if matrix[i][j] == 1 and j == 0:
+				count += col + 1
+			else:
+				count += col - j
 			i += 1
-		else:
+		elif matrix[i][j] == 1:
 			j -= 1
+
 	return count
 
 print(oneMatrix(matrix))
