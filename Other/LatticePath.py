952fc94e757140995272d6ee24fdc73293ffa896
Llorch <SkylakeBeta@gmail.com>
2017-03-02 23:37:37 -0800

    Add test to Lattice Path PY

--- Other/LatticePath.py
+++ Other/LatticePath.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def latticePath(x, y):
 	if x == 0 and y == 0:
 		return 1
@@ -5,3 +7,13 @@ def latticePath(x, y):
 		return 0
 
 	return latticePath(x-1, y) + latticePath(x, y-1)
+
+class LatticePathTest:
+	def test(self, func):
+		assert_equal(func(2, 2), 6)
+		assert_equal(func(0, 0), 1)
+		assert_equal(func(3, 2), 10)
+		print('TESTS PASSED')
+
+t = LatticePathTest()
+t.test(latticePath)
--- Recursion/ReverseString.py
+++ Recursion/ReverseString.py
@@ -5,12 +5,12 @@ def reverseString(s):
 		return s
 	return reverseString(s[1:]) + s[0]
 
-class reverseStringTest:
+class ReverseStringTest:
 	def test(self, func):
 		assert_equal(func("String"), "gnirtS")
 		assert_equal(func("123456"), "654321")
 		assert_equal(func("madam"), "madam")
 		print("TESTS PASSED")
 
-t = reverseStringTest()
+t = ReverseStringTest()
 t.test(reverseString)
