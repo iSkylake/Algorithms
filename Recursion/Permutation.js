4b6955206f5f2a4cbdd180adc308f4f154c12666
Llorch <SkylakeBeta@gmail.com>
2017-04-01 22:36:44 -0700

    Add Permutation JS

---
+++ Recursion/Permutation.js
@@ -0,0 +1,21 @@
+function permutation(arr){
+	if(arr.length <= 1){
+		return arr;
+	}
+
+	var perms = [];
+
+	for(i=0; i<arr.length; i++){
+		var char = arr[i];
+
+		var rem = arr.slice(0,i) + arr.slice(i+1, arr.length);
+
+		for(var p of permutation(rem)){
+			perms.push(char+p);
+		}
+	}
+
+	return perms;
+}
+
+console.log(permutation('123'));
