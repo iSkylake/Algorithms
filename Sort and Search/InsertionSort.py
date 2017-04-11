981bbc12cc1b6b13597ee59c34a538e536e9be0c
Llorch <SkylakeBeta@gmail.com>
2017-04-10 22:55:32 -0700

    Test Insertion Sort PY

--- Sort and Search/InsertionSort.py
+++ Sort and Search/InsertionSort.py
@@ -1,3 +1,5 @@
+from nose.tools import assert_equal
+
 def insertion_sort(arr):
 	for i in range(len(arr)):
 		k = i
@@ -6,4 +8,12 @@ def insertion_sort(arr):
 			k -= 1
 	return arr
 
-print(insertion_sort([5, 4, 1, 9, 7, 6]))
+class Sort_Test:
+	def test(self, func):
+		assert_equal(func([5, 4, 1, 9, 7, 6]), [1, 4, 5, 6, 7, 9])
+		assert_equal(func([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6])
+		assert_equal(func([9, 7, 5, 3, 2, 1]), [1, 2, 3, 5, 7, 9])
+		print('TESTS PASSED')
+
+t = Sort_Test()
+t.test(insertion_sort)
