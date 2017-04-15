ec357231a6d7564c76bb4bf10e4b3d52c4679c66
Llorch <SkylakeBeta@gmail.com>
2017-04-14 17:42:57 -0700

    Clean Island Count JS

--- Array/IslandCount.js
+++ Array/IslandCount.js
@@ -2,27 +2,20 @@ function islandCount(matrix){
 	var island = 0;
 	for(i=0; i<matrix.length; i++){
 		for(j=0; j<matrix[i].length; j++){
-			if(i === matrix.length-1){
-				if(matrix[i][j+1] === 0 && matrix[i][j] === 1){
+			if(matrix[i][j] === 1){
+				if(i === matrix.length-1 && j === matrix[i].length-1 || i === matrix.length-1 && matrix[i][j+1] == 0 || j === matrix[i].length-1 && matrix[i+1][j] == 0 || matrix[i][j+1] === 0 && matrix[i+1][j] === 0 || matrix[i+1][j] === 0 && matrix[i][j+1] === 0){
 					island++;
 				}
-			} else if(j === matrix[i].length-1){
-				if(matrix[i+1][j] === 0 && matrix[i][j] === 1){
-					island++;
-				}
-			} else if(i === matrix.length-1 && j === matrix[i].length-1 && matrix[i][j] === 1){
-				island++;
-			} else if(matrix[i+1][j] === 0 && matrix[i][j+1] === 0 && matrix[i][j] === 1){
-				island++;
 			}
 		}
 	}
 	return island;
 }
+	
 
-map = [	[1, 1, 0, 1],
-		[1, 1, 0, 1],
-		[0, 0, 1, 0],
-		[0, 0, 1, 0] ]
+var map = [	[1, 1, 0, 1],
+			[1, 1, 0, 1],
+			[0, 0, 1, 0],
+			[0, 0, 1, 0] ]
 
 console.log(islandCount(map));
