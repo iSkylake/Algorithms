c984369247c61d0395ee8c918cd45715afc1cd1a
Llorch <skylakebeta@gmail.com>
2017-05-20 00:24:45 -0700

    Add QueryToObject JS

---
+++ Other/QueryToObject.js
@@ -0,0 +1,14 @@
+let query = 'htttp://www.randomSite.com/?name=hana&time=43627543&favorite=mech,starcraft,stream'
+
+function queryToObj(query){
+	let urlList = query.split("?");
+	let queryList = urlList[1].split("&");
+	let obj = {};
+	for(let i=0; i<queryList.length; i++){
+		let item = queryList[i].split("=");
+		obj[item[0]] = item[1]
+	}
+	console.log(obj);
+}
+
+queryToObj(query);
