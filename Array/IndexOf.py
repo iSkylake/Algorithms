f91f88e7be53eb0724b9fc90f0510cb2e032d9d6
Llorch <skylakebeta@gmail.com>
2017-05-19 23:58:26 -0700

    Add IndexOf PY

---
+++ Array/IndexOf.py
@@ -0,0 +1,22 @@
+def indexOf(target, string):
+	target = target.lower()
+	string = string.lower()
+
+	for i in range(len(string)):
+
+		if(string[i] == target[0]):
+			index = i
+			subIndex = 0
+
+			while string[index] == target[subIndex]:
+				index += 1
+				subIndex += 1
+
+			if subIndex == len(target):
+				return i
+
+	return "-1"
+
+string = "Pirate Warrior so OP, just go face SMORC"
+
+print(indexOf("War", string))
