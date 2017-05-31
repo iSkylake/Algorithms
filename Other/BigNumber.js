1be7e1027dc5d1c08760cd34f4395ac94adf3de6
Llorch <skylakebeta@gmail.com>
2017-05-30 22:58:06 -0700

    Add substraction to Big Number JS

--- Other/BigNumber.js
+++ Other/BigNumber.js
@@ -27,6 +27,25 @@ BigNumber.prototype.add = function(value){
 	return sum;
 };
 
+BigNumber.prototype.substract = function(value){
+	let a = this.value.split("").reverse().map(Number);
+	let b = value.split("").reverse().map(Number);
+	let maxLen = Math.max(a.length, b.length);
+	let sub = [];
+	for(let i=0; i<maxLen; i++){
+		a[i] = a[i] || 0;
+		b[i] = b[i] || 0;
+		if(a[i]<b[i]){
+			a[i]+=10;
+			a[i+1] -= 1;
+		}
+		sub.push(a[i]-b[i]);
+	}
+	sub = sub.reverse().join("");
+	return sub;
+}
+
 let bigNum = new BigNumber('6537');
 
 console.log(bigNum.add('9856'));
+console.log(bigNum.substract('4671'));
