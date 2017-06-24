771da1ebb3ba3f2d4beb1180b8d72b45f5a5bc89
Llorch <skylakebeta@gmail.com>
2017-06-23 23:34:52 -0700

    Finish SuperBST JS

--- Tree/SuperBST.js
+++ Tree/SuperBST.js
@@ -33,10 +33,25 @@ BST.prototype.add = function(value){
 	this.size++;
 };
 
+function superBST(root){
+	let prev = 0;
+	function traverse(node){
+		if(node !== null){
+			traverse(node.right);
+			node.value += prev;
+			prev = node.value;
+			traverse(node.left);
+		}
+	}
+	traverse(root);
+};
+
 let bst = new BST();
 bst.add(10);
 bst.add(5);
 bst.add(1);
 bst.add(15);
 
+console.log(bst.root);
+superBST(bst.root);
 console.log(bst.root);
