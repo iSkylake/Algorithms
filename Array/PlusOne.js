83d9e7cf710f38f4e1a2249c99ecffe6bfcc66c6
iSkylake <skylakebeta@gmail.com>
2018-05-11 18:03:55 -0700

    Optimize PlusOne and add test

--- Array/PlusOne.js
+++ Array/PlusOne.js
@@ -1,25 +1,33 @@
-function plusOne(arr){
-	var len = arr.length
-	if(arr[len-1] < 9){
-		arr[len-1]++;
-		return (arr);
-	}
-	var i = len-1;
-	while(arr[i] === 9){
-		if(i===0){
+function addOne(arr){
+	let i = arr.length - 1;
+	if(arr[i] < 9){
+		arr[i] += 1;
+	} else {
+		while(arr[i] === 9 && i >= 0){
 			arr[i] = 0;
-			arr.unshift(1);
-			return arr;
-		} else if(arr[i-1] < 9){
-			arr[i] = 0;
-			arr[i-1]++;
+			i--;
 		}
-		arr[i] = 0;
-		i--;
+		if(i === -1){
+			arr.unshift(1);
+		} else {
+			arr[i] += 1;
+		}
 	}
 	return arr;
 }
 
-console.log(plusOne([1, 0, 0]));
-console.log(plusOne([9, 9]));
-console.log(plusOne([1, 9, 9]));
+// INPUT
+let input = [[1, 2], [1, 9], [1, 2, 9], [9], [9, 9, 9]];
+let correctOutput = [[1, 3], [2, 0], [1, 3, 0], [1, 0], [1, 0, 0, 0]];
+
+// TESTING
+for(let i=0; i<input.length; i++){
+	let result = "";
+	let output = addOne(input[i]).toString();
+	if(correctOutput[i].toString() === output.toString()){
+		result = "PASSED";
+	} else {
+		result = "FAILED";
+	}
+	console.log(`${input[i]} => ${output} ${result}`);
+};
